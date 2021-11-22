c516227e3e06d7589f0be5f0d4e1bfaed3ef6f04
j09180ga <ghassan.alkulaibi@student.manchester.ac.uk>
2021-11-22 02:26:41 +0000

    Setting up gitlab repo

---
+++ python_game.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from random import randint
+
+windowWidth = 800
+windowHeight = 700
+direction = ['left', 'right']
+
+window = Tk()
+canvas = Canvas(window, width=windowWidth, height=windowHeight, bg='#857d7a')
+canvas.pack()
+
+
+
+
+window.mainloop()
