5334171baa430b4d1942fc48d95f406d2f1668dd
j09180ga <ghassan.alkulaibi@student.manchester.ac.uk>
2021-11-22 03:36:52 +0000

    Reszing images and testing design theories

--- python_game.py
+++ python_game.py
@@ -1,15 +1,31 @@
 from tkinter import *
-from random import randint
+from random import randint, choice
+from time import sleep
 
 windowWidth = 800
 windowHeight = 700
-direction = ['left', 'right']
+playerLives = 5
+direction = ['Ambulance', 'Audi', 'Black_viper', 'Car', 'Mini_truck', 'Mini_van', 'Police', 'taxi', 'truck']
 
 window = Tk()
 canvas = Canvas(window, width=windowWidth, height=windowHeight, bg='#857d7a')
 canvas.pack()
+for i in range(100, windowWidth, 150):
+    canvas.create_line(i, 0, i, windowHeight)
 
+livesTxt = canvas.create_text(10, 10, text='Lives: '+str(playerLives), font=('Aerial', 15), anchor='nw')
 
+vehicleImage = PhotoImage(file="images/Car.png")
+vehicleCanvas = canvas.create_image(175, 500, image=vehicleImage)
 
+while True:
+    pos = canvas.coords(vehicleCanvas)
+    if pos[1] < 0:
+        canvas.delete(vehicleCanvas)
+        break
+    canvas.move(vehicleCanvas, 0, -10)
+    sleep(0.02)
+    window.update()
 
 window.mainloop()
+
