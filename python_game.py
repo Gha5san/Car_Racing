8f11187dc0cfaac9feb861b4a55a7dc1d6322a6b
j09180ga <ghassan.alkulaibi@student.manchester.ac.uk>
2021-11-24 09:52:13 +0000

    Restructuring the program, adding a front page

--- python_game.py
+++ python_game.py
@@ -4,6 +4,7 @@ from time import sleep, time
 
 pauseState    = False
 livesMode     = False
+cheatIsOn     = False
 vehicleNumbers = 0
 maxVehicleNumber  = 6
 miniVehicleNumber = 0
@@ -13,18 +14,28 @@ windowHeight  = 700
 vehicleWidth  = 55
 vehicleHeight = 128
 defualtSpeed  = 10
-playerDefualtSpeed = defualtSpeed
+playerDefualtSpeed = 3
 playerLives   = 5
 vehicleOption = ['Ambulance', 'Audi', 'Black_viper', 'Car', 'Mini_truck', 'Mini_van', 'Police', 'taxi', 'truck']
+
+
+# window = Tk()
+# myCanvas = Canvas(window, width=windowWidth, height=windowHeight, bg='#857d7a')
+# myCanvas.pack()
+# for i in range(100, windowWidth-99, 75):
+#     myCanvas.create_line(i, 0, i, windowHeight, dash=(10,3))
+#
+# livesText = myCanvas.create_text(10, 10, text='Lives: '+str(playerLives), font=('Aerial', 15), anchor='nw')
+# scoreText = myCanvas.create_text(100, 10, text='Score: 0', font=('Aerial', 15), anchor='nw')
 
 window = Tk()
 myCanvas = Canvas(window, width=windowWidth, height=windowHeight, bg='#857d7a')
 myCanvas.pack()
-for i in range(100, windowWidth-99, 75):
-    myCanvas.create_line(i, 0, i, windowHeight, dash=(10,3))
+for i in range(100, windowWidth - 99, 75):
+    myCanvas.create_line(i, 0, i, windowHeight, dash=(10, 3))
 
-livesText = myCanvas.create_text(10, 10, text='Lives: '+str(playerLives), font=('Aerial', 15), anchor='nw')
-timeText = myCanvas.create_text(100, 10, text='Time: 0', font=('Aerial', 15), anchor='nw')
+livesText = myCanvas.create_text(10, 10, text='Lives: ' + str(playerLives), font=('Aerial', 15), anchor='nw')
+scoreText = myCanvas.create_text(100, 10, text='Score: 0', font=('Aerial', 15), anchor='nw')
 
 class Vehicle():
 
@@ -135,15 +146,6 @@ def player_decrease_speed(event):
     global playerDefualtSpeed
     playerDefualtSpeed -= 2
 
-playerVehicle = Vehicle(startingPoint[0], startingPoint[1], "Car", "player", "player/" )
-myCanvas.bind_all('<Up>', playerVehicle.dir_up)
-myCanvas.bind_all('<Down>', playerVehicle.dir_down)
-myCanvas.bind_all('<Left>', playerVehicle.dir_left)
-myCanvas.bind_all('<Right>', playerVehicle.dir_right)
-myCanvas.bind_all('<a>', player_decrease_speed)
-myCanvas.bind_all('<d>', player_increase_speed)
-myCanvas.bind_all('<p>', lambda x: pause("Pause"))
-
 vehicleOpposite = []
 loop = True
 def create_vehicle():
@@ -165,30 +167,66 @@ def delete_vehicle():
             myCanvas.delete(i.draw)
             i.state = "Deleted"
 
-# create_vehicle()
+def change_vehicle():
+    global  chosenVehicle, vehicleChoice, vehicleButton, vehicleButtonWindow
+    vehicleIndex = vehicleOption.index(chosenVehicle)
+    if vehicleIndex == 8:
+        vehicleIndex = - vehicleIndex
+    chosenVehicle = vehicleOption[vehicleIndex + 1]
+    vehicleChoice = PhotoImage(file="images/player/" + chosenVehicle + ".png")
+    myCanvas.delete(vehicleButtonWindow)
+    vehicleButton = Button(window, image=vehicleChoice, borderwidth=0, bg="#857d7a", command=change_vehicle, activebackground="#857d7a")
+    vehicleButtonWindow = myCanvas.create_window(336, 590, anchor=NW, window=vehicleButton)
+    if selectVehicle in myCanvas.find_all(): myCanvas.delete(selectVehicle)
+
+def cheat_mode_on(event):
+    global cheatIsOn
+    cheatIsOn = True
+def cheat_mode_off(event):
+    global cheatIsOn
+    cheatIsOn = False
+def intiating():
+    global playerVehicle
+
+    myCanvas.delete(startButtonWindow)
+    myCanvas.delete(vehicleButtonWindow)
+    if selectVehicle in myCanvas.find_all(): myCanvas.delete(selectVehicle)
+    playerVehicle = Vehicle(startingPoint[0], startingPoint[1], chosenVehicle, "player", "player/")
+    myCanvas.bind_all('<Up>', playerVehicle.dir_up)
+    myCanvas.bind_all('<Down>', playerVehicle.dir_down)
+    myCanvas.bind_all('<Left>', playerVehicle.dir_left)
+    myCanvas.bind_all('<Right>', playerVehicle.dir_right)
+    myCanvas.bind_all('<a>', player_decrease_speed)
+    myCanvas.bind_all('<d>', player_increase_speed)
+    myCanvas.bind_all('<p>', lambda x: pause("Pause"))
+    myCanvas.bind_all("<KeyPress-c>",   cheat_mode_on)
+    myCanvas.bind_all("<KeyRelease-c>", cheat_mode_off)
+
+    main_code()
+
 timeStamps = []
 startTime = time()
 unusedTime = 0
 totatUnusedTime = 0
 def main_code():
-    global pauseState, playerLives, timeText, unusedTime, totatUnusedTime
+    global pauseState, playerLives, scoreText, unusedTime, totatUnusedTime, score
     while (not pauseState):
         if unusedTime != 0:
             unusedTime = time() - unusedTime
             totatUnusedTime += unusedTime
         elapsedTime = time() - (startTime + totatUnusedTime)
-        myCanvas.itemconfig(timeText, text=f'Time: {round(elapsedTime, 2)}')
+        score = (elapsedTime * defualtSpeed) / 10
+        myCanvas.itemconfig(scoreText, text=f'Score: {round(score, 2)}')
         elapsedTime = int(elapsedTime)
-        if elapsedTime % randint(2, 5) == 0 and elapsedTime not in timeStamps:
+        if elapsedTime % 2 == 0 and elapsedTime not in timeStamps:
             timeStamps.append(elapsedTime)
-            if len(myCanvas.find_withtag("bots")) >= 2*maxVehicleNumber:
+            if len(myCanvas.find_withtag("bots")) >= 2 * maxVehicleNumber:
                 delete_vehicle()
             create_vehicle()
         unusedTime = 0
 
-
-        #update player vehicle position
-        myCanvas.move(playerVehicle.draw, 0, -playerDefualtSpeed+9)
+        # update player vehicle position
+        myCanvas.move(playerVehicle.draw, 0, -playerDefualtSpeed)
         playerVehicle.position_update()
         if playerVehicle.get_position()[1] < -50:
             myCanvas.delete(playerVehicle.draw)
@@ -202,17 +240,34 @@ def main_code():
                 continue
             myCanvas.move(i.draw, 0, defualtSpeed)
             # print(len(myCanvas.find_withtag("bots")))
-            if i.draw in myCanvas.find_withtag("bots") and collision(playerVehicle.get_position(), i.get_position()):
-                playerLives -= 1
-                myCanvas.itemconfig(livesText, text='Lives: '+str(playerLives))
-                pause(f"You have {playerLives} lives left")
+            if not cheatIsOn:
+                if i.draw in myCanvas.find_withtag("bots") and collision(playerVehicle.get_position(), i.get_position()):
+                    playerLives -= 1
+                    myCanvas.itemconfig(livesText, text='Lives: ' + str(playerLives))
+                    pause(f"You have {playerLives} lives left")
 
         sleep(0.02)
         window.update()
     else:
         unusedTime = time()
 
-main_code()
+chosenVehicle = choice(vehicleOption)
+vehicleChoice = PhotoImage(file="images/player/" + chosenVehicle +".png")
+vehicleButton = Button(window, image=vehicleChoice, borderwidth=0, bg="#857d7a", command=change_vehicle, activebackground="#857d7a")
+vehicleButtonWindow = myCanvas.create_window(336, 590, anchor=NW, window=vehicleButton)
+selectVehicle = myCanvas.create_text(370, 580, text='Click to switch vehicle: ', font=('Aerial', 15))
 
+startButton = Button(window, text="Start", font=("Aerial", 40), borderwidth=0, bg="#857d7a", command=intiating)
+startButtonWindow = myCanvas.create_window(335, 200, window=startButton)
+
+
+# intiating()
 window.mainloop()
+
+
+
+
+
+
+
 
